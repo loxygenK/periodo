e4509ad3c89581ceeba87256e6005f5c414e82e8
loxygenk <loxygen.k@gmail.com>
2021-01-16 13:46:41 +0900

    🚚 Moved app.tsx to proper directory

--- src/comps/app.tsx
+++ src/comps/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import style from "./style/test.module.scss";
+import style from "../style/test.module.scss";
 
 const App: React.FC = () => (
   <div className={style.test}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
 
-import App from "./app";
+import App from "./comps/app";
 
 render(
   <StrictMode>
